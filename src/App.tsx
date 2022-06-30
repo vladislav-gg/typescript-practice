ff1dcfd4122c677e598927f4664539e0dc5abbd2
Vladislav <selies@mail.ru>
2022-06-30 01:45:07 +0100

    some styling added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<body>
+			<div className="App">
+				<header className="App-header">
+					<div className="text-wrapper">
+						<div className="h1-wrapper">
+							<h1>Where in the world?</h1>
+							<h2>Dark mode</h2>
+						</div>
+					</div>
+				</header>
+			</div>
+			<section className="main-section">
+				<div className="main-wrapper">
+					<input id="country-input" placeholder="Search for a country"></input>
+					<select name="continents" id="continents">
+						<option value="filter">Filter by Region</option>
+						<option value="africa">Africa</option>
+						<option value="america">America</option>
+						<option value="asia">Asia</option>
+						<option value="europe">Europe</option>
+						<option value="oceania">Oceania</option>
+					</select>
+				</div>
+			</section>
+		</body>
+	);
 }
 
 export default App;
